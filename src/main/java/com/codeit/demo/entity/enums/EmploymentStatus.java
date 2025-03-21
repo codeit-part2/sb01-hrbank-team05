8fcdf28a2fc909cf6adcc88503e29ab84dd01a4c
soyul <soyul9280@gmail.com>
2025-03-21 15:22:56 +0900

    [Fix]EmploymentStatus string 타입 추가

--- src/main/java/com/codeit/demo/entity/enums/EmploymentStatus.java
+++ src/main/java/com/codeit/demo/entity/enums/EmploymentStatus.java
@@ -1,7 +1,15 @@
 package com.codeit.demo.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum EmploymentStatus {
-  ACTIVE,
-  ON_LEAVE,
-  RESIGNED
+  ACTIVE("ACTIVE"),
+  ON_LEAVE("ON_LEAVE"),
+  RESIGNED("RESIGNED");
+
+  private String value;
+  EmploymentStatus(String value) {
+    this.value = value;
+  }
 }
